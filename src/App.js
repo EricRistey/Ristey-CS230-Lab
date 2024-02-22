33a8db9f92997fb346ef7010f6c9d3d0a9d9f38a
EricRistey <ejr00015@mix.wvu.edu>
2024-02-22 11:29:59 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>CS 230L</h1>
+    <h2>Section 001</h2>
+    <p>WVU ID: 800386734</p>
+    <p>Hi I am Eric Ristey</p>
     </div>
   );
 }
